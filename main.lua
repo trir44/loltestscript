7b7d925ae2862c8cad24da322a4e2f1fab94f9a2
Chase Wilson <120544075+trir44@users.noreply.github.com>
2023-08-18 10:14:03 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,19 @@
+local Code = [[
+
+]]
+
+throwoffvariables = {"Haha", "Security and Shit", "LemonSec v3?", "All code raw?"}
+local security = true
+
+if security == true 
+  then
+end
+
+
+if security = true 
+then
+local encoded = Code:gsub(".", function(bb) return "\\" .. bb:byte() end) or thing .. "\""
+  print('ls = loadstring')
+print('ls("'..encoded..'")()')
+  print('math.random')
+end)
